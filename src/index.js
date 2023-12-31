6aa1c59608745cec3bf549a7539e654ee224f4cb
Arfa Zia <arfazia2006@gmail.com>
2023-12-31 18:00:45 +0500

    Call all the functions for DOM

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import './style.css';
 import * as webpage from './dom func.js';
 webpage.Navbar();
-// webpage.HomePage();
-// webpage.StoryPage();
-// webpage.Menu();
-webpage.ContactUs();
+webpage.HomePage();
+
+const navbarLinks = document.querySelectorAll('.navbar-links');
+navbarLinks.forEach(element => element.addEventListener('click', (event) => {
+    let linkId = event.target.id;
+    switch(linkId){
+        case "link1" :
+            ClearMainPage();
+            webpage.HomePage();
+            break;
+        case "link2":
+            ClearMainPage();
+            webpage.StoryPage();
+            break;
+            case "link3" :
+            ClearMainPage();
+            webpage.Menu();
+            break;
+        case "link4" :
+            ClearMainPage();
+            webpage.ContactUs();
+            break;
+        }
+}));
+function ClearMainPage(){
+    const content = document.querySelector('#main-content');
+    content.textContent = " ";
+}
 
